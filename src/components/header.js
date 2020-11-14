196a6dcde8e052efe4f8579ef23c5c5f76e446dd
Matt Lindsey <matthew.r.lindsey@gmail.com>
2020-11-14 13:17:39 -0500

    add login and signup links

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,33 @@
 import React from "react"
-
+import { Link } from "gatsby"
 import logo from "../../static/logo.png"
 
 const Header = () => {
   return (
     <header className="flex h-16 bg-header">
-      <a href="/" className="shadow-none flex-1 text-center px-4 py-2 m-2">
+      <Link to="/" className="shadow-none flex-1 text-center px-4 py-2 m-2">
         <img id="logo" data-testid="logo" src={logo} alt="Agile Ventures" />
-      </a>
+      </Link>
+      <ul style={{ listStyle: `none`, float: `right` }}>
+        <li
+          style={{
+            display: `inline-block`,
+            marginRight: `1rem`,
+            marginTop: "10%",
+          }}
+        >
+          <Link to="/login/">Log In</Link>
+        </li>
+        <li
+          style={{
+            display: `inline-block`,
+            marginRight: `1rem`,
+            marginTop: "10%",
+          }}
+        >
+          <Link to="/signup/">Sign Up</Link>
+        </li>
+      </ul>
     </header>
   )
 }
