e6fdb3f217200c51a0f65d038a0be10611c17bda
Matt Lindsey <matthew.r.lindsey@gmail.com>
2020-11-14 22:58:29 -0500

    add ListLink to header

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,14 @@ import React from "react"
 import { Link } from "gatsby"
 import logo from "../../static/logo.png"
 
+const ListLink = ({ to, text }) => (
+  <li
+    style={{ display: `inline-block`, marginRight: `1rem`, marginTop: "10%" }}
+  >
+    <Link to={to}>{text}</Link>
+  </li>
+)
+
 const Header = () => {
   return (
     <header className="flex h-16 bg-header">
@@ -9,24 +17,8 @@ const Header = () => {
         <img id="logo" data-testid="logo" src={logo} alt="Agile Ventures" />
       </Link>
       <ul style={{ listStyle: `none`, float: `right` }}>
-        <li
-          style={{
-            display: `inline-block`,
-            marginRight: `1rem`,
-            marginTop: "10%",
-          }}
-        >
-          <Link to="/login/">Log In</Link>
-        </li>
-        <li
-          style={{
-            display: `inline-block`,
-            marginRight: `1rem`,
-            marginTop: "10%",
-          }}
-        >
-          <Link to="/signup/">Sign Up</Link>
-        </li>
+        <ListLink to="/login/" text="Log In" />
+        <ListLink to="/signup/" text="Sign Up" />
       </ul>
     </header>
   )
