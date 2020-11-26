9c3d248a2e6203569db0f417a0cc94290acd0667
Matt Lindsey <matthew.r.lindsey@gmail.com>
2020-11-26 11:43:06 -0500

    add test for dynamic eventlist

--- src/tests/components/eventlist.test.js
+++ src/tests/components/eventlist.test.js
@@ -1,11 +1,11 @@
 import React from "react"
 import renderer from "react-test-renderer"
 import { PureEventList as EventList } from "../../components/eventlist"
-import events from "../fixtures/events"
+import upcomingEvents from "../fixtures/upcoming-events"
 
 describe("EventList", () => {
   it("renders correctly", () => {
-    const tree = renderer.create(<EventList data={events} />).toJSON()
+    const tree = renderer.create(<EventList data={upcomingEvents} />).toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
