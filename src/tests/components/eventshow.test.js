97f35321c3c927c505aa4dd46e8cd654f03493c9
Matt Lindsey <matthew.r.lindsey@gmail.com>
2020-12-02 18:54:17 -0500

    add nextevent fixture and fix test

--- src/tests/components/eventshow.test.js
+++ src/tests/components/eventshow.test.js
@@ -1,11 +1,11 @@
 import React from "react"
 import renderer from "react-test-renderer"
 import { PureEventShow as EventShow } from "../../components/eventshow"
-import event from "../fixtures/event"
+import nextEvent from "../fixtures/nextevent"
 
 describe("EventShow", () => {
   it("renders correctly", () => {
-    const tree = renderer.create(<EventShow data={event} />).toJSON()
+    const tree = renderer.create(<EventShow data={nextEvent} />).toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
---
+++ src/tests/fixtures/nextevent.js
@@ -0,0 +1,18 @@
+const nextEvent = {
+  nextEvent: {
+    event: {
+      id: "86",
+      category: "Scrum",
+      duration: 800,
+      name: "Premium event",
+      repeats: "weekly",
+      startDatetime: "2020-11-21T16:45:00Z",
+      timeZone: "UTC",
+      description:
+        "Architecto molestiae quo. Eos nisi ab. Alias modi cupiditate.",
+    },
+    time: "2020-12-03T16:45:00Z",
+  },
+}
+
+export default nextEvent
